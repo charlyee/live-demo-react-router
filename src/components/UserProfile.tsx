53f635c6b48d5ae30bba8d66e67247470b280aa6
Cole Thomas Evans <cevans2@ualberta.ca>
2020-04-23 13:02:37 -0600

    update

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -11,9 +11,34 @@ export interface IUserProfileProps extends RouteComponentProps<IRouteParams> {
 
 export default class UserProfile extends React.Component<IUserProfileProps> {
     public render() {
+        const { match: { params: { id } } } = this.props;
+
+        // const { match } = this.props;
+
+        // const { params } = match;
+
+        // const { id } = params;
+
+        // Location of id param: this.props.match.params.id;
+
+        // this.props.match.params.id;
+
+        // console.log(id);
+
+        // let var2 = id+2;
+
+
+        // this.props.userList 
+
+        // let userToDisplayProfile = userList.filter(individualUser => {
+        //     return individualUser.id === id
+        // })[0]; //undefined??? null???
+
+
+
         return (
             <div>
-                User PRofile and ID Passed in the URL is 0
+                User PRofile and ID Passed in the URL is {id}
             </div>
         );
     }
